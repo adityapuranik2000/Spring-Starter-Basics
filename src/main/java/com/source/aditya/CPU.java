c9e263aa81618118d1e72022281278c930fd6516
Aditya Puranik <adityapuranik36@gmail.com>
2026-02-05 17:03:55 +0530

    Autowired

---
+++ src/main/java/com/source/aditya/CPU.java
@@ -0,0 +1,11 @@
+package com.source.aditya;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CPU {
+
+    public void load(){
+        System.out.println("Gearing up...");
+    }
+}
--- src/main/java/com/source/aditya/Laptop.java
+++ src/main/java/com/source/aditya/Laptop.java
@@ -1,11 +1,17 @@
 package com.source.aditya;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component //This annotation defines that Spring has to manage the bean creation and destruction for this class
 public class Laptop {
 
+    @Autowired // This annotation injects the bean of CPU class here.
+    CPU cpu;
+
     public void start(){
-        System.out.println("Starting...");
+
+        cpu.load();
+        System.out.println("CPU geared up now opening the laptop..");
     }
 }
